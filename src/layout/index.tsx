262e15a2832fbe0126020f7b07bcdbb934b228e3
rupeshshandilya <rupeshkshandilya@gmail.com>
2023-05-06 22:22:52 +0530

    🎨 style(layout): improve layout structure and spacing The layout structure has been improved by adding a max width, minimum height, and centering content. The spacing between the header and main content has been increased to improve readability. The main content is now wrapped in a VStack to ensure proper spacing and alignment.

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -6,10 +6,15 @@ type Props = PropsWithChildren<{}>;
 const Layout = ({ children }: Props) => {
     return (
         <>
-        <Container display="flex">
-            <VStack flex={1}>
+        <Container display="flex"   maxW="container.md"
+      minH={{ base: "auto", md: "100vh" }}
+      px={{ base: 4, lg: 0 }}
+      centerContent>
+            <VStack flex={1} spacing={16} alignItems="stretch" w="full">
                 <Header />
+                <VStack spacing={16} flex={1} w="full" as="main">
                 {children}
+                </VStack>
             </VStack>
         </Container>
         </>
